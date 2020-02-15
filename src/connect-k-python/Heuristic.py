27fcc0a04ee1b1bfb8fc5731c099d76aa7275e1c
yundai424 <yundai424@gmail.com>
2020-02-14 19:30:08 -0800

    add threat; modify TARGET_DEPTH based on param

--- src/connect-k-python/Heuristic.py
+++ src/connect-k-python/Heuristic.py
@@ -2,6 +2,8 @@ from typing import *
 from enum import Enum
 from BoardClasses import *
 
+MAX_VALUE = 99999
+
 
 class Player(Enum):
     empty = 0
@@ -29,14 +31,23 @@ class Heuristic:
         self.weights = weights
 
     def eval_board(self, player: Player, board: Board) -> float:
+        # score = 0
+        # if self.weights[0] != 0:
+        #     # p1 winning rows & diags
+        #     score += self.weights[0] * self.__winning_chances(player, board)
+        # if self.weights[1] != 0:
+        #     # p2 winning rows & diags
+        #     score += self.weights[1] * self.__winning_chances(player.opposite, board)
+        # return score
         score = 0
-        if self.weights[0] != 0:
-            # p1 winning rows & diags
-            score += self.weights[0] * self.__winning_chances(player, board)
-        if self.weights[1] != 0:
-            # p2 winning rows & diags
-            score += self.weights[1] * self.__winning_chances(player.opposite, board)
-        return score
+        r_chances, r_threats = Heuristic.__count_row(player, board)
+        c_chances, c_threats = Heuristic.__count_col(player, board)
+        d_chances, d_threats = Heuristic.__count_diag(player, board, True)
+        ad_chances, ad_threats = Heuristic.__count_diag(player, board, False)
+        return self.weights[0] * (r_chances[player.value] + c_chances[player.value] + d_chances[player.value] + ad_chances[player.value]) + \
+               self.weights[1] * (r_chances[player.opposite.value] + c_chances[player.opposite.value] + d_chances[player.opposite.value] + ad_chances[player.opposite.value]) + \
+               self.weights[2] * (r_threats[player.value] + c_threats[player.value] + d_threats[player.value] + ad_threats[player.value]) + \
+               self.weights[3] * (r_threats[player.opposite.value] + c_threats[player.opposite.value] + d_threats[player.opposite.value] + ad_threats[player.opposite.value])
 
     @staticmethod
     def __winning_chances(player: Player, board: Board):
@@ -81,10 +92,86 @@ class Heuristic:
                     score += 1
         return score
 
+    @staticmethod
+    def __count_row(player: Player, board: Board):
+        chances = {1: 0, 2: 0}
+        threats = {1: 0, 2: 0}
+        for x in range(board.row):
+            count = {0: 0, 1: 0, 2: 0}
+            for y in range(board.k):
+                count[board.board[x][y]] += 1
+            y = board.k - 1
+            while True:
+                Heuristic.__eval_counts(player, board, count, chances, threats)
+                # slide window
+                y += 1
+                if y >= board.col:
+                    break
+                count[board.board[x][y - board.k]] -= 1
+                count[board.board[x][y]] += 1
+        return chances, threats
+
+    @staticmethod
+    def __count_col(player: Player, board: Board):
+        chances = {1: 0, 2: 0}
+        threats = {1: 0, 2: 0}
+        for y in range(board.col):
+            count = {0: 0, 1: 0, 2: 0}
+            for x in range(board.k):
+                count[board.board[x][y]] += 1
+            x = board.k - 1
+            while True:
+                Heuristic.__eval_counts(player, board, count, chances, threats)
+                # slide window
+                x += 1
+                if x >= board.row:
+                    break
+                count[board.board[x - board.k][y]] -= 1
+                count[board.board[x][y]] += 1
+        return chances, threats
+
+    @staticmethod
+    def __count_diag(player: Player, board: Board, anti_diag: bool):
+        chances = {1: 0, 2: 0}
+        threats = {1: 0, 2: 0}
+        range_row = range(board.row - board.k + 1) if not anti_diag else range(board.k - 1, board.row)
+        dx = 1 if not anti_diag else -1
+
+        for start_x in range_row:
+            count = {0: 0, 1: 0, 2: 0}
+            for i in range(board.k):
+                count[board.board[start_x + dx * i][i]] += 1
+            y = board.k - 1
+            while True:
+                Heuristic.__eval_counts(player, board, count, chances, threats)
+                y += 1
+                if y >= board.col:
+                    break
+                count[board.board[start_x + dx * (y - board.k)][y - board.k]] -= 1
+                count[board.board[start_x + dx * y][y]] += 1
+            return chances, threats
+
+
+    @staticmethod
+    def __eval_counts(player: Player, board: Board, count: Dict[int, int], chances: Dict[int, int],
+                      threats: Dict[int, int]):
+        if count[player.value] == board.k:  # have already won, no bother further searching
+            chances[player.value] = MAX_VALUE
+            return
+        if count[player.value] > 0 and count[player.opposite.value] == 0:
+            chances[player.value] += 1
+            if count[player.value] == board.k - 1:
+                threats[player.value] += 1
+        if count[player.opposite.value] > 0 and count[player.value] == 0:
+            chances[player.opposite.value] += 1
+            if count[player.opposite.value] == board.k - 1:
+                threats[player.opposite.value] += 1
+
 
 if __name__ == "__main__":
-    b = Board(3, 3, 2, 0)
+    b = Board(5, 5, 3, 0)
     b.board[0][0] = 1
     b.board[1][0] = 1
-    h = Heuristic([1, -1, 0, 0, 0, 0])
+    b.show_board(None)
+    h = Heuristic([1, -1, 0.1, -0.1, 0, 0])
     print(h.eval_board(Player(1), b))
